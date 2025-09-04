05fc15fa3cbad59205a8035938e0654e4eb8c712
NirojBhattarai <neerajbhttri@gmail.com>
2025-09-04 17:58:14 +0545

    feat: Visa Sub Module UI Component Design

--- src/pages/visaProcess/visa/Visa.tsx
+++ src/pages/visaProcess/visa/Visa.tsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import Table from '@/components/Table';
-import useDisclosure from '@/hooks/useDisclousre';
 import { VisaColumns } from './partials/VisaColumns';
 import { visaTableData } from '@/data/visa';
-import AddVisa from './partials/AddVisa';
 import PageHeader from '@/common/PageHeader';
+import Breadcrumb from '@/components/reusable-component/Breadcrumb';
+import VisaFilterList from './partials/VisaFilterList';
+import { PATH } from '@/constant/path';
 
 const Visa: React.FC = () => {
-  const addModal = useDisclosure();
-
   return (
     <div className="bg-surface w-full min-h-full">
-      <div className="px-5">
+      <Breadcrumb Navone="Dashboard" Navtwo="Visa" />
+      <div>
         <div className="w-full h-fit">
-          <PageHeader
-            title="Visa"
-            handleAddClick={addModal?.toggle}
-            routePath="/"
-          />
+          <PageHeader title="Visa" routePath={PATH.visa.addVisa} />
         </div>
-
+        <div className="py-5">
+          <VisaFilterList />
+        </div>
         <div className="overflow-x-visible">
           <Table columns={VisaColumns} data={visaTableData} />
         </div>
       </div>
-
-      <AddVisa isOpen={addModal?.isOpen} handleCloseModal={addModal?.close} />
     </div>
   );
 };
---
+++ src/pages/visaProcess/visa/partials/VisaFilterList.tsx
@@ -0,0 +1,11 @@
+import SearchSection from '@/components/reusable-component/SearchSection';
+
+const VisaFilterList = () => {
+  return (
+    <div>
+      <SearchSection showAdjustmentIcon={false} styleClass="w-72 h-10" />
+    </div>
+  );
+};
+
+export default VisaFilterList;
