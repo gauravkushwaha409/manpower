dc506b21188527af9aaf1c6523ed12e09aa71101
NirojBhattarai <neerajbhttri@gmail.com>
2025-09-04 17:50:21 +0545

    feat: Job Application Sub Module Form, Hook and UI Components Design

---
+++ src/pages/jobs/jobApplication/partials/JobApplicationActions.tsx
@@ -0,0 +1,36 @@
+import { DeleteIcon, EditIcon } from '@/components/actions/TableComp';
+import useDisclosure from '@/hooks/useDisclousre';
+import DeleteModal from '@/components/DeleteModal';
+import { PATH } from '@/constant/path';
+
+interface TableActionsProps<T> {
+  row: T;
+  className?: string;
+}
+
+function JobApplicationActions<T>({
+  className = 'flex items-center gap-4 ml-5',
+}: TableActionsProps<T>) {
+  const deleteModal = useDisclosure();
+
+  const handleDelete = () => {
+    deleteModal?.open();
+  };
+
+  return (
+    <>
+      <div className={className}>
+        <div>
+          <EditIcon updateRoutePath={PATH.jobProcess.updateJobApplication} />
+        </div>
+        <div onClick={handleDelete}>
+          <DeleteIcon />
+        </div>
+      </div>
+
+      <DeleteModal isOpen={deleteModal.isOpen} onCancel={deleteModal.close} />
+    </>
+  );
+}
+
+export default JobApplicationActions;
