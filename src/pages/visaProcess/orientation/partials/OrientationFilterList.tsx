cca178cee14295bee25cbfc7ad6f3b19e8b128b7
NirojBhattarai <neerajbhttri@gmail.com>
2025-09-04 17:58:55 +0545

    feat: Oreintation Sub Module UI Component Design

---
+++ src/pages/visaProcess/orientation/partials/OrientationFilterList.tsx
@@ -0,0 +1,11 @@
+import SearchSection from '@/components/reusable-component/SearchSection';
+
+const OrientationFilterList = () => {
+  return (
+    <div>
+      <SearchSection showAdjustmentIcon={false} styleClass="w-72 h-10" />
+    </div>
+  );
+};
+
+export default OrientationFilterList;
