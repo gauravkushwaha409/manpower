6692edb74fcbfda5692b1ea768de562167bed483
NirojBhattarai <neerajbhttri@gmail.com>
2025-09-04 17:56:54 +0545

    feat: Emabassy Interview Module UI Component Design

--- src/pages/visaProcess/medicalReport/MedicalReport.tsx
+++ src/pages/visaProcess/medicalReport/MedicalReport.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import Table from '@/components/Table';
-import useDisclosure from '@/hooks/useDisclousre';
-import AddMedicalReport from './partials/AddMedicalReport';
 import { medicalReportTableData } from '@/data/medicalReport';
 import { MedicalReportColumns } from './partials/MedicalReportColumns';
 import PageHeader from '@/common/PageHeader';
+import { PATH } from '@/constant/path';
+import Breadcrumb from '@/components/reusable-component/Breadcrumb';
+import MedicalReportFilterList from './partials/MedicalReportFilterList';
 
 const MedicalReport: React.FC = () => {
-  const addModal = useDisclosure();
-
   return (
     <div className="bg-surface w-full min-h-full">
-      <div className="px-5">
+      <Breadcrumb Navone="Dashboard" Navtwo="Embassy Interview" />
+      <div>
         <div className="w-full h-fit">
           <PageHeader
             title="Medical Report"
-            handleAddClick={addModal?.toggle}
-            routePath="/"
+            routePath={PATH.visa.addMedicalReport}
           />
         </div>
-
+        <div className="py-5">
+          <MedicalReportFilterList />
+        </div>
         <div className="overflow-x-visible">
           <Table columns={MedicalReportColumns} data={medicalReportTableData} />
         </div>
       </div>
-
-      <AddMedicalReport
-        isOpen={addModal?.isOpen}
-        handleCloseModal={addModal?.close}
-      />
     </div>
   );
 };
