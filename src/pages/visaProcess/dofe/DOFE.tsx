6fdafe2b390f18d2349796996f4a80f92dff5c98
NirojBhattarai <neerajbhttri@gmail.com>
2025-09-04 17:57:51 +0545

    feat: DOFE Sub Module UI Component Design

--- src/pages/visaProcess/dofe/DOFE.tsx
+++ src/pages/visaProcess/dofe/DOFE.tsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import Table from '@/components/Table';
-import useDisclosure from '@/hooks/useDisclousre';
-import AddDOFE from './partials/AddDOFE';
 import { dofeTableData } from '@/data/dofe';
 import { DOFEColumns } from './partials/DOFEColumns';
 import PageHeader from '@/common/PageHeader';
+import Breadcrumb from '@/components/reusable-component/Breadcrumb';
+import DOFEFilterList from './partials/DOFEFilterList';
+import { PATH } from '@/constant/path';
 
 const Dofe: React.FC = () => {
-  const addModal = useDisclosure();
-
   return (
     <div className="bg-surface w-full min-h-full">
-      <div className="px-5">
+      <Breadcrumb
+        Navone="Dashboard"
+        Navtwo="Department of Foreign Employment (DOFE)"
+      />
+      <div>
         <div className="w-full h-fit">
           <PageHeader
-            title="DOFE (Department of Foreign Employment)"
-            handleAddClick={addModal?.toggle}
-            routePath="/"
+            title="Department of Foreign Employment (DOFE)"
+            routePath={PATH.visa.addDOFE}
           />
+        </div>
+        <div className="py-5">
+          <DOFEFilterList />
         </div>
 
         <div className="overflow-x-visible">
           <Table columns={DOFEColumns} data={dofeTableData} />
         </div>
       </div>
-
-      <AddDOFE isOpen={addModal?.isOpen} handleCloseModal={addModal?.close} />
     </div>
   );
 };
