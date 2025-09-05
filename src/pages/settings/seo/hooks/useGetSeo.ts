c6d9733a623ca53ab1a0eb275dd098beb39ddae5
suyogstha7 <suyogstha317@gmail.com>
2025-09-05 16:21:20 +0545

    feat: Add Hook to Create, Update and Get Seo and Userdefined Types for Seo Sub Module

---
+++ src/pages/settings/seo/hooks/useGetSeo.ts
@@ -0,0 +1,12 @@
+import { ISeo } from "../interface/ISeo";
+
+export const SeoTableData: ISeo[] = [
+  {
+    id: "1",
+    meta_title: "Suyog Shrestha",
+    meta_description: "suyogstha@gmail.com",
+    og_title: "Hello, its me suyog",
+    og_description: "Hello, its me suyog",
+    seo_type: "Home"
+  },
+];
---
+++ src/pages/settings/seo/interface/ISeo.ts
@@ -0,0 +1,8 @@
+export interface ISeo {
+  id: string;
+  meta_title: string;
+  meta_description: string;
+  og_title: string;
+  og_description: string;
+  seo_type: string;
+}
