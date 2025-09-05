7afc8300a258e4115188d7b1135d73ba0431c4fd
suyogstha7 <suyogstha317@gmail.com>
2025-09-05 12:47:54 +0545

    feat: Add Hook to Create, Update, Delete and Get Locations and User Defined Types for Location Sub Module

---
+++ src/pages/settings/location/hooks/useGetLocation.ts
@@ -0,0 +1,9 @@
+import { ILocation } from "../interface/ILocation";
+
+export const LocationTableData: ILocation[] = [
+  {
+    id: "1",
+    locationName: "Suman Bhatt",
+    locationImage: "Dome",
+  },
+];
