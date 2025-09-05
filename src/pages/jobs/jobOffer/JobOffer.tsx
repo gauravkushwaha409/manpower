82e8f9d03a67736f838793f2b5b39eea972d7933
NirojBhattarai <neerajbhttri@gmail.com>
2025-09-05 10:15:42 +0545

    ref: Refactor Create and Update Job Offer Hooks, Offer Form, and Job Offer Page

--- src/pages/jobs/jobOffer/JobOffer.tsx
+++ src/pages/jobs/jobOffer/JobOffer.tsx
@@ -1,123 +1,29 @@
 import React from 'react';
-import Table, { CustomColumnDef } from '../../../components/Table';
 import PageHeader from '@/common/PageHeader';
-import {
-  DeleteIcon,
-  EditIcon,
-  ViewIcon,
-} from '@/components/actions/TableComp.tsx';
-import { IJobOffer } from './interface/IJobOffer';
-import useJobOffer from './hooks/useJobOffer';
-import useCreateJobOffer from './hooks/useCreateJobOffer';
-import useUpdateJobOffer from './hooks/useUpdateJobOffer';
-import AddJobOfferModal from './modal/AddJobOfferModal';
-import UpdateJobOfferModal from './modal/UpdateJobOfferModal';
-import DeleteModal from '@/components/DeleteModal';
+import Breadcrumb from '@/components/reusable-component/Breadcrumb';
+import { PATH } from '@/constant/path';
+import JobOfferFilterList from './partials/JobOfferFilterList';
+import Table from '@/components/Table';
+import { JobOfferColumns } from './partials/JobOfferColumn';
 import { jobOfferTableData } from './hooks/useGetJobOffer';
 
 const JobOffer: React.FC = () => {
-  const {
-    addJobOffer,
-    updateJobOffer,
-    setUpdateJobOffer,
-    deleteJobOffer,
-    setDeleteJobOffer,
-    handleOpenAddModal,
-    handleCloseAddModal,
-    handleDeleteJobOffer,
-    handleCloseDeleteModal,
-    handleCloseUpdateModal,
-  } = useJobOffer();
-  const { addJobOfferFormik } = useCreateJobOffer();
-  const { updateJobOfferFormik } = useUpdateJobOffer();
-
-  const tableHead: CustomColumnDef<IJobOffer>[] = [
-    {
-      header: 'Company Name',
-      accessorKey: 'company_name',
-      search: true,
-    },
-    {
-      header: 'Candidate Name',
-      accessorKey: 'candidate_name',
-      search: true,
-    },
-    {
-      header: 'Job Vacancy',
-      accessorKey: 'job_vacancy',
-      search: true,
-    },
-    {
-      header: 'Offer Date',
-      accessorKey: 'offer_date',
-      search: false,
-    },
-    {
-      header: 'Salary Offered',
-      accessorKey: 'salary_offered',
-      search: false,
-    },
-    {
-      header: 'Status',
-      accessorKey: 'status',
-      search: false,
-    },
-    {
-      header: 'Action',
-      accessorKey: 'action',
-      search: false,
-      cell: ({ row }) => (
-        <div className="flex items-center gap-4 ml-5">
-          <ViewIcon id={row.original.id} />
-          <button
-            onClick={() => {
-              setUpdateJobOffer(row.original.id);
-            }}
-          >
-            <EditIcon updateRoutePath="#" />
-          </button>
-          <button
-            onClick={() => {
-              setDeleteJobOffer(row.original.id);
-            }}
-          >
-            <DeleteIcon />
-          </button>
-        </div>
-      ),
-    },
-  ];
-
   return (
-    <div className="bg-surface rounded-lg w-full min-h-full">
-      <div className="relative px-6">
+    <div className="bg-surface w-full min-h-full">
+      <Breadcrumb Navone="Dashboard" Navtwo="Job Offer" />
+      <div>
         <div className="w-full h-fit">
           <PageHeader
-            title="Job Offers"
-            handleAddClick={handleOpenAddModal}
-            routePath="/"
+            title="Job Offer"
+            routePath={PATH.jobProcess.addJobOffer}
           />
         </div>
-
-        <div className="overflow-auto">
-          <Table columns={tableHead} data={jobOfferTableData} />
+        <div className="py-5">
+          <JobOfferFilterList />
         </div>
-
-        <AddJobOfferModal
-          formik={addJobOfferFormik}
-          handleCloseModal={handleCloseAddModal}
-          isOpen={addJobOffer}
-        />
-        <UpdateJobOfferModal
-          formik={updateJobOfferFormik}
-          handleCloseModal={handleCloseUpdateModal}
-          isOpen={!!updateJobOffer}
-        />
-        <DeleteModal
-          isOpen={!!deleteJobOffer}
-          onCancel={handleCloseDeleteModal}
-          onConfirm={handleDeleteJobOffer}
-        />
+        <div className="overflow-x-visible">
+          <Table columns={JobOfferColumns} data={jobOfferTableData} />
+        </div>
       </div>
     </div>
   );
