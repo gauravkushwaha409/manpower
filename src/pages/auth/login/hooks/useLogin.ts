2a1f4b3a7a36d57d50e8c465d1bfc7b42f41f543
NirojBhattarai <neerajbhttri@gmail.com>
2025-09-14 11:06:53 +0545

    update: Login Form Hook

--- src/pages/auth/login/hooks/useLogin.ts
+++ src/pages/auth/login/hooks/useLogin.ts
@@ -1,48 +1,58 @@
 import { usePostDataMutation } from '@/api/api';
-import { loginUser } from '@/store/authSlice';
+import { endpoints } from '@/api/endpoints';
 import { showErrorMessage, showSuccessMessage } from '@/utils/toast';
 import { useFormik } from 'formik';
-import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { formSchema, LoginFormValues } from '../schema/loginValidation';
+import * as Yup from 'yup';
+import { setCookie } from '@/utils/cookie';
+import { ILoginError, ILoginSuccess } from '../interface/ILogin';
+import { PATH } from '@/constant/path';
 
 const useLogin = () => {
- const navigate = useNavigate();
- const dispatch = useDispatch();
- const [login, { isLoading, isSuccess, isError }] = usePostDataMutation();
+  const navigate = useNavigate();
+  const [login, { isLoading, isSuccess, isError }] = usePostDataMutation();
 
- const initialValues: LoginFormValues = {
-  email: '',
-  password: '',
- };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().required('Email is required'),
+      password: Yup.string()
+        .required('Password is required')
+        .min(4, 'Password must be at least 6 characters'),
+      //  .matches(/(?=.*[A-Z])/, "Password must contain at least one uppercase letter")
+      //  .matches(/(?=.*\d)/, "Password must contain at least one number")
+      //  .matches(/(?=.*[!@#$%^&*()_+={}\[\]:;"\'<>,.?/\\|`~])/, "Password must contain at least one special character"),
+    }),
+    onSubmit: async (values) => {
+      const res = await login({
+        url: endpoints.login,
+        data: values,
+      });
+      const response: ILoginSuccess = res?.data;
+      console.log(response, 'Response Data');
+      const error = res?.error as ILoginError;
+      if (response && response?.status === 'success') {
+        console.log('Login Successful');
+        setCookie({
+          cookieName: 'accessToken',
+          value: response?.data?.accessToken,
+        });
+        setCookie({
+          cookieName: 'refreshToken',
+          value: response?.data?.refreshToken,
+        });
+        navigate(PATH.dashboard.dashboard);
+        showSuccessMessage(response?.message);
+      } else if (error) {
+        showErrorMessage(error?.data?.message);
+      }
+    },
+  });
 
- const formik = useFormik({
-  initialValues,
-  validationSchema: formSchema,
-  onSubmit: async (values) => {
-   const res = await login({
-    url: '',
-    data: values,
-   });
-   const response = res?.data;
-   const error = res?.error;
-   if (response && response?.success) {
-    dispatch(
-     loginUser({
-      accessToken: response?.token?.access,
-      refreshToken: response?.token?.refresh,
-      userId: response?.user?.id,
-      isManpowerLoggedIn: true,
-     })
-    );
-    navigate('/');
-    showSuccessMessage(response?.message);
-   } else if (error && 'message' in error) {
-    showErrorMessage(error?.message || 'Some error occured');
-   }
-  },
- });
-
- return { formik, isLoading, isSuccess, isError };
+  return { formik, isLoading, isSuccess, isError };
 };
+
 export default useLogin;
