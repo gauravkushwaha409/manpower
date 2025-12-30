641272b34969e07c44dbc7f99e99cdf8bb0dd5e3
Gaurav Kushwaha <gaurav.kushwaha.0825@gmail.com>
2025-12-30 23:51:04 +0545

    chore: remove old notification modal

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,11 +1,9 @@
-import NotificationModal from "@/components/NotificationModal";
 import ProfileSection from "./partials/ProfileSection";
 
 const Header = () => {
   return (
     <header className="bg-white u-gap-x flex justify-end items-center">
       <div className="flex items-center gap-4">
-        <NotificationModal />
         <ProfileSection />
       </div>
     </header>
