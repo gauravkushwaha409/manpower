ad70bf70dab711c1734e16351037d675cd20ede1
Gaurav Kushwaha <gaurav.kushwaha.0825@gmail.com>
2025-12-30 23:38:54 +0545

    chore: delete old notification module

--- src/pages/notification/interface/INotification.ts
+++
@@ -1,8 +0,0 @@
-export interface INotification {
-  id: number;
-  iconType: "UserPlus" | "FileText" | "Globe" | "Languages"; 
-  title: string;
-  description: string;
-  time: string;
-  read?: boolean;
-}
